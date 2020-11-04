fc050189716df4a57cc65ca4e063b7ed95a7b536
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2020-11-04 10:38:09 +0300

    Users endpoint

--- app/Filters/UserFiltrator.php
+++ app/Filters/UserFiltrator.php
@@ -3,17 +3,28 @@ namespace App\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class UserFiltrator {
 
     public function apply(Builder $queryBuilder, Request $request) {
 
         if ($request->filled('is_author')) {
-            $queryBuilder->has('articles');
+            $isAuthor = filter_var($request->get('is_author'), FILTER_VALIDATE_BOOLEAN);
+            if ($isAuthor) {
+                $queryBuilder->has('articles');
+            } else {
+                $queryBuilder->doesntHave('articles');
+            }
         }
 
         if ($request->filled('is_published')) {
-            $queryBuilder->published();
+            $isPublished = filter_var($request->get('is_published'), FILTER_VALIDATE_BOOLEAN);
+            if  ($isPublished) {
+                $queryBuilder->published();
+            } else {
+                $queryBuilder->unpublished();
+            }
         }
 
         return $queryBuilder;
