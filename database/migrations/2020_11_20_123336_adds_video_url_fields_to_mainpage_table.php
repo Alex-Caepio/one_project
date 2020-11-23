463764158ee5422fe44a87334d43325c2550eab9
Michael Tokar <m.tokar@amdersenlab.com>
2020-11-23 09:17:44 +0200

    Fixes the migration

--- database/migrations/2020_11_20_123336_adds_video_url_fields_to_mainpage_table.php
+++ database/migrations/2020_11_20_123336_adds_video_url_fields_to_mainpage_table.php
@@ -18,7 +18,6 @@ class AddsVideoUrlFieldsToMainpageTable extends Migration
             $table->renameColumn('section_10_media_url', 'section_10_image_url');
             $table->renameColumn('section_11_media_url', 'section_11_image_url');
 
-            $table->string('section_1_video_url')->after('section_1_image_url')->nullable();
             $table->string('section_6_video_url')->after('section_6_media_url')->nullable();
             $table->string('section_10_video_url')->after('section_10_media_url')->nullable();
             $table->string('section_11_video_url')->after('section_11_media_url')->nullable();
@@ -34,7 +33,6 @@ class AddsVideoUrlFieldsToMainpageTable extends Migration
     {
         Schema::table('main_pages', function (Blueprint $table) {
             $table->dropColumn([
-                'section_1_video_url',
                 'section_6_video_url',
                 'section_10_video_url',
                 'section_11_video_url'
