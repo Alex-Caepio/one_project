a186d486b853c5041f44e07be15c37380eae955a
Michael Tokar <m.tokar@amdersenlab.com>
2020-12-29 12:16:44 +0200

    Changes timezone controller to not have pagination

--- app/Http/Controllers/TimezoneController.php
+++ app/Http/Controllers/TimezoneController.php
@@ -11,18 +11,12 @@ class TimezoneController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Timezone::query();
+        $timezones = Timezone::with($request->getIncludes())->all();
 
-        $includes = $request->getIncludes();
-        $paginator = $query->with($includes)
-            ->paginate($request->getLimit());
-
-        $timezone = $paginator->getCollection();
-
-        return response(
-            fractal($timezone, new TimezoneTransformer())
+        return
+            fractal($timezones, new TimezoneTransformer())
                 ->parseIncludes($request->getIncludes())
-        )->withPaginationHeaders($paginator);
+                ->respond();
     }
 }
 
