7add2dfbbc83e74da4d827a386bdfa06dc0f5ed0
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2020-12-31 07:55:47 +0000

    Countries

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Actions\Country\CountryFilter;
+use App\Filters\CountryFiltrator;
 use App\Http\Requests\Request;
+use App\Models\Country;
 use App\Transformers\CountryTransformer;
 
-class CountryController extends Controller
-{
-    public function filter(Request $request){
-        $country = run_action(CountryFilter::class, $request);
-        return fractal($country, new CountryTransformer())->respond();
+class CountryController extends Controller {
+
+    public function index(Request $request) {
+        $countryQuery = Country::query();
+        $countryFilter = new CountryFiltrator();
+        $countryFilter->apply($countryQuery, $request);
+        return fractal($countryQuery->get(), new CountryTransformer())->respond();
+
     }
 }
