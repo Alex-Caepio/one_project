1a2a79900dd45f3e3c73afa3c9b0e2a23d474f6f
jar1ax <jar1ax.dev@gmail.com>
2021-01-13 13:05:16 +0200

    PlanUpdateRequest added and PlanController updated

---
+++ app/Http/Requests/Admin/PlanUpdateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use App\Http\Requests\Request;
+
+class PlanUpdateRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'   => 'required|max:20',
+            'price' => 'required_unless:is_free,true|nullable|integer|gte:0',
+            'description' => 'max:150',
+            'trial_months' => 'nullable|integer',
+            'commission_on_sale' => 'gte:0|lte:100',
+            'schedules_per_service' => 'gte:1|lte:20',
+            'pricing_options_per_service' => 'gte:1|lte:20',
+            'article_publishing' => 'gte:1|lte:20'
+        ];
+    }
+
+}
