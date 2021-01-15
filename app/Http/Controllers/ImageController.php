6613ee2315c57d7b88418aa53a32bc967920cc9e
jar1ax <jar1ax.dev@gmail.com>
2021-01-15 13:11:05 +0200

    Store path refactored

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -18,7 +18,7 @@ class ImageController extends Controller
     {
         $ImageStorage = config('image.image_storage');
 
-        $path = Storage::disk($ImageStorage)->put('images/originals', $request->file);
+        $path = Storage::disk($ImageStorage)->put('/tmp', $request->file);
         $request->merge([
             'size' => $request->file->getClientSize(),
             'path' => $path
