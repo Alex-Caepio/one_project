9e5d83c1829a027c2edc7613cf547906324c2c97
jar1ax <jar1ax.dev@gmail.com>
2021-01-18 14:59:26 +0200

    ImageController updated with new URL requirements

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,8 +10,9 @@ class ImageController extends Controller
     public function upload(ImageUploadRequest $request)
     {
         $image = Storage::disk(config('image.image_storage'))->put('tmp', $request->file);
-
-        $url = Storage::url($image);
+        config('image.image_storage') != 'local'?
+        $url = Storage::url($image) :
+        $url = env('APP_URL') . Storage::url($image);
 
         return response()->json(['url' => $url]);
     }
