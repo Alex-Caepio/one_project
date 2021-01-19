d72b5ff577582caf1856375c6091640deb8da1f3
jar1ax <jar1ax.dev@gmail.com>
2021-01-19 10:23:30 +0200

    ImageController refactored

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,9 +10,9 @@ class ImageController extends Controller
     public function upload(ImageUploadRequest $request)
     {
         $image = Storage::disk(config('image.image_storage'))->put('tmp', $request->file);
-        config('image.image_storage') != 'local'?
-        $url = Storage::url($image) :
-        $url = env('APP_URL') . Storage::url($image);
+        $url= config('image.image_storage') != 'public'
+            ? Storage::url($image)
+            : env('APP_URL') . Storage::url($image);
 
         return response()->json(['url' => $url]);
     }
