3b655227ae0a23e333ae71341fbd11d701690eec
jar1ax <jar1ax.dev@gmail.com>
2021-01-25 09:42:29 +0200

    AuthController refactored

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -17,13 +17,11 @@ use App\Models\Schedule;
 use App\Models\User;
 use App\Transformers\UserTransformer;
 use DB;
-use Doctrine\DBAL\Exception\NotNullConstraintViolationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
-use PHPUnit\Runner\Exception;
 use Stripe\StripeClient;
 
 
