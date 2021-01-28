9bb86cca2bf10bef07ed428531af37ae64d7d3e8
jar1ax <jar1ax.dev@gmail.com>
2021-01-28 13:10:05 +0200

    hasMediaItems Trait added to Articles components

--- app/Actions/Article/ArticleStore.php
+++ app/Actions/Article/ArticleStore.php
@@ -15,25 +15,6 @@ class ArticleStore extends ArticleAction {
      */
     public function execute(ArticleRequest $request): Article {
         $article = new Article();
-
-        if (!empty($request->image_url))
-        {
-            $image = Storage::disk(config('image.image_storage'))
-                ->put("/images/artcles/{$article->id}/media_images/", file_get_contents($request->image_url));
-            $request->image_url = Storage::url($image);
-        }
-
-        if (!empty($request->media_images))
-        {
-            foreach ($request->media_images as $mediaImage)
-            {
-                $image = Storage::disk(config('image.image_storage'))
-                    ->put("/images/artcles/{$article->id}/media_images/", file_get_contents($mediaImage['url']));
-                $mediaImage[] = Storage::url($image);
-            }
-            $request->media_images = $mediaImage;
-        }
-
         $this->saveArticle($article, $request);
         return $article;
     }
--- app/Actions/Article/ArticleUpdate.php
+++ app/Actions/Article/ArticleUpdate.php
@@ -7,7 +7,6 @@ namespace App\Actions\Article;
 use App\Http\Requests\Articles\ArticleRequest;
 use App\Models\Article;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class ArticleUpdate extends ArticleAction {
 
@@ -16,48 +15,8 @@ class ArticleUpdate extends ArticleAction {
      * @param \App\Models\Article $article
      */
     public function execute(ArticleRequest $request, Article $article) {
-        if ($request->filled('media_images') && !empty($request->media_images))
-        {
-            foreach ($request->media_images as $mediaImage)
-            {
-                if (Storage::disk(config('image.image_storage'))->missing(file_get_contents($mediaImage['url'])))
-                {
-                    $image = Storage::disk(config('image.image_storage'))
-                        ->put("/images/articles/{$article->id}/media_images/", file_get_contents($mediaImage['url']));
-                    $image_urls[]['url'] = Storage::url($image);
-                }
-            }
-            $request->media_images = $image_urls;
-        }
+
         $this->saveArticle($article, $request);
-        if ($request->filled('media_images') && !empty($request->media_images)){
-            $article->media_images()->whereNotIn('url', $request->media_images)->delete();
-            $urls = collect($request->media_images)->pluck('url');
-            $recurringURL = $article->media_images()->whereIn('url', $urls)->pluck('url')->toArray();
-            $newImages = $urls->filter(function($value) use ($recurringURL) {
-                return !in_array($value, $recurringURL);
-            });
-
-            foreach ($newImages as $url){
-                $imageUrlToStore[]['url'] = $url;
-            }
-
-            $article->media_images()->createMany($imageUrlToStore);
-        }
-        if ($request->filled('media_videos') && !empty($request->media_videos)) {
-            $article->media_videos()->whereNotIn('url', $request->media_videos)->delete();
-            $urls = collect($request->media_videos)->pluck('url');
-            $recurringURL = $article->media_videos()->whereIn('url', $urls)->pluck('url')->toArray();
-            $newVideos = $urls->filter(function($value) use ($recurringURL) {
-                return !in_array($value, $recurringURL);
-            });
-
-            foreach ($newVideos as $url){
-                $videoUrlToStore[]['url'] = $url;
-            }
-
-            $article->media_videos()->createMany($videoUrlToStore);
-        }
         return $article;
     }
 
