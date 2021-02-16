24e09b0bc5ee5b2b67b951f1cf89d775da7913a6
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-02-16 11:37:50 +0000

    MailSender abstract

---
+++ app/Listeners/Emails/SendEmailHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Listeners\Emails;
+
+use App\EmailVariables\EmailVariables;
+use App\Models\CustomEmail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+abstract class SendEmailHandler {
+
+    protected string $templateName;
+    protected string $toEmail;
+    protected object $event;
+
+    abstract public function handler(): void;
+
+    protected function sendCustomEmail(): void {
+        try {
+            $emailData = CustomEmail::where('name', $this->templateName)->first();
+            if ($emailData) {
+                $body = $emailData->text;
+                $emailVariables = new EmailVariables($this->event);
+                $bodyReplaced = $emailVariables->replace($body);
+                Mail::raw($bodyReplaced, function($message) {
+                    $message->to($this->toEmail);
+                });
+            } else {
+                throw new \RuntimeException('Email template #' . $this->templateName . ' was not found');
+            }
+        } catch (\Exception $e) {
+            Log::channel('emails')->info('Email error: ', [
+                'template'   => $this->templateName,
+                'event_name' => get_class($this->event),
+                'user_email' => $this->toEmail,
+                'message'    => $e->getMessage(),
+            ]);
+        }
+    }
+
+
+}
