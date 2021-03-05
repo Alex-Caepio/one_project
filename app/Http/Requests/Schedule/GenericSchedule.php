3518d865afad85b62ff5b189a71d65e17494c0d6
Michael Tokar <m.tokar@amdersenlab.com>
2021-03-05 09:46:40 +0200

    Adds name fields for schedule files, adds better validation on schedule prices

--- app/Http/Requests/Schedule/GenericSchedule.php
+++ app/Http/Requests/Schedule/GenericSchedule.php
@@ -16,7 +16,7 @@ class GenericSchedule extends Request implements CreateScheduleInterface
      */
     public function authorize()
     {
-        return (bool) $this->user()->plan;
+        return (bool)$this->user()->plan;
     }
 
     /**
@@ -29,27 +29,43 @@ class GenericSchedule extends Request implements CreateScheduleInterface
         return [];
     }
 
-   public function withValidator($validator): void {
-       $validator->after(function($validator) {
-           $plan = $this->user()->plan;
-           $totalSchedules = $this->service->schedules()->count();
+    public function withValidator($validator): void
+    {
+        $validator->after(function ($validator) {
+            $plan           = $this->user()->plan;
+            $totalSchedules = $this->service->schedules()->count();
 
-           if (!$plan->unlimited_bookings) {
-                $this->validate(['attendees' => "max:{$this->user()->plan->amount_bookings}"]);
-           }
+            if (!$plan->unlimited_bookings && $this->attendees > $plan->amount_bookings) {
+                $validator->errors()->add('attendees', "You're limited to {$plan->amount_bookings} attendees");
+            }
 
-//            if(!$plan->list_paid_services) {
-//                $validator->errors()->add('prices.*.cost', 'Your plan restricts you from publishing paid services.');
-//            }
-//
-//            if(!$plan->list_free_services) {
-//                $this->validate(['prices.*.cost' => 'min:0|not_in:0']);
-//                $validator->errors()->add('prices.*.cost', 'Your plan restricts you from publishing free services.');
-//            }
+            //if user not allowed to list paid services
+            if (!$plan->list_paid_services && $this->has('prices') && $this->hasPaidPrices()) {
+                $validator->errors()->add('prices.*.cost', 'Your plan restricts you from publishing paid services.');
+            }
 
-            if(!$plan->schedules_per_service_unlimited && $totalSchedules >= $plan->schedules_per_service){
+            //if user not allowed to list free services
+            if (!$plan->list_free_services && $this->has('prices') && $this->hasFreePrices()) {
+                $validator->errors()->add('prices.*.cost', 'Your plan restricts you from publishing free services.');
+            }
+
+            if (!$plan->schedules_per_service_unlimited && $totalSchedules >= $plan->schedules_per_service) {
                 $validator->errors()->add('service_id', 'The schedules limit on the service has been exceeded.');
             }
-       });
-   }
+        });
+    }
+
+    protected function hasPaidPrices()
+    {
+        return !collect($this->prices)->filter(function ($item) {
+            return isset($item['cost']) && $item['cost'] > 0;
+        })->isEmpty();
+    }
+
+    protected function hasFreePrices()
+    {
+        return !collect($this->prices)->filter(function ($item) {
+            return isset($item['cost']) && in_array($item['cost'], [0, null]);
+        })->isEmpty();
+    }
 }
