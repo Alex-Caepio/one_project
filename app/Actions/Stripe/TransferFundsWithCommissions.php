d40eb4ac3cbb81a351ff149e6997225cb6448063
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-03-24 13:51:38 +0300

    Transfer exception handler

--- app/Actions/Stripe/TransferFundsWithCommissions.php
+++ app/Actions/Stripe/TransferFundsWithCommissions.php
@@ -4,22 +4,20 @@ namespace App\Actions\Stripe;
 
 use App\Models\PractitionerCommission;
 use App\Models\Transfer;
+use Illuminate\Support\Facades\Log;
 use Stripe\StripeClient;
 
 
-class TransferFundsWithCommissions
-{
-    public function execute($cost, $practitoner, $schedule = null)
-    {
+class TransferFundsWithCommissions {
+    public function execute($cost, $practitoner, $schedule = null) {
         $stripe = app()->make(StripeClient::class);
 
         $practitionerPlan = $practitoner->plan->commission_on_sale;
 
-        $practitionerCommissions = PractitionerCommission::where('practitioner_id', $practitoner->id)
-            ->where(function ($q) {
-                $q->where('is_dateless', true)
-                    ->orWhereRaw('date_from <= NOW() AND date_to >= NOW()');
-            })->get();
+        $practitionerCommissions =
+            PractitionerCommission::where('practitioner_id', $practitoner->id)->where(function($q) {
+                    $q->where('is_dateless', true)->orWhereRaw('date_from <= NOW() AND date_to >= NOW()');
+                })->get();
 
         $reductions[] = $cost * $practitionerPlan / 100;
 
@@ -31,37 +29,24 @@ class TransferFundsWithCommissions
         foreach ($reductions as $reduction) {
             $amount -= $reduction;
         }
-        try {
-            $stripe->transfers->create([
-                'amount'      => $amount,
-                'currency'    => config('app.platform_currency'),
-                'destination' => $practitoner->stripe_account_id,
-            ]);
 
-            $transfer                    = new Transfer();
-            $transfer->user_id           = $practitoner->id;
-            $transfer->stripe_account_id = $practitoner->stripe_account_id;
-            $transfer->status            = 'success';
-            $transfer->amount            = $amount;
-            $transfer->amount_original   = $cost;
-            $transfer->currency          = config('app.platform_currency');
-            $transfer->schedule_id       = $schedule->id ?? null;
-            $transfer->description       = 'transfer for a schedule purchase';
-            $transfer->save();
-        } catch (\Stripe\Exception\ApiErrorException $e) {
-            $transfer                    = new Transfer();
-            $transfer->user_id           = $practitoner->id;
-            $transfer->stripe_account_id = $practitoner->stripe_account_id;
-            $transfer->status            = 'fail';
-            $transfer->amount            = $amount;
-            $transfer->amount_original   = $cost;
-            $transfer->currency          = config('app.platform_currency');
-            $transfer->schedule_id       = $schedule->id ?? null;
-            $transfer->description       = 'transfer for a schedule purchase';
-            $transfer->save();
+        $stripe->transfers->create([
+                                       'amount'      => $amount,
+                                       'currency'    => config('app.platform_currency'),
+                                       'destination' => $practitoner->stripe_account_id,
+                                   ]);
 
-            return abort(500);
-        }
+        $transfer = new Transfer();
+        $transfer->user_id = $practitoner->id;
+        $transfer->stripe_account_id = $practitoner->stripe_account_id;
+        $transfer->status = 'success';
+        $transfer->amount = $amount;
+        $transfer->amount_original = $cost;
+        $transfer->currency = config('app.platform_currency');
+        $transfer->schedule_id = $schedule->id ?? null;
+        $transfer->description = 'transfer for a schedule purchase';
+        $transfer->save();
+        
 
         return $transfer;
     }
