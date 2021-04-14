07c376cc40e33a4bec1b00144bfd4a31e7dacf6b
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-04-14 10:20:18 +0100

    422 error on cancel

--- app/Http/Requests/Cancellation/CancelBookingRequest.php
+++ app/Http/Requests/Cancellation/CancelBookingRequest.php
@@ -31,13 +31,4 @@ class CancelBookingRequest extends FormRequest {
         return [];
     }
 
-    public function withValidator($validator) {
-        $validator->after(function($validator) {
-            $this->booking->load('schedule');
-            if (Carbon::parse($this->booking->schedule->start_date) < Carbon::now()) {
-                $validator->errors()->add('error', 'The schedule cannot be cancelled');
-            }
-        });
-    }
-
 }
