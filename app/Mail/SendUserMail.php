ab80b982b532e9a2a7da3c32cbc1357158421f9a
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-04-30 13:59:48 +0100

    From remove

--- app/Mail/SendUserMail.php
+++ app/Mail/SendUserMail.php
@@ -43,8 +43,7 @@ class SendUserMail extends Mailable {
             $this->sender->isPractitioner() ? config('app.platform_subject_practitioner') : config('app.platform_subject_client');
         $subject .= $this->sender->isPractitioner() ? $this->sender->business_name : $this->sender->first_name . ' ' .
                                                                                      $this->sender->last_name;
-        return $this->from($this->sender->email, $this->sender->first_name . ' ' . $this->sender->last_name)
-                    ->subject($subject)->view('mails.send_user', [
+        return $this->subject($subject)->view('mails.send_user', [
                                                                    'text'     => $this->text,
                                                                    'sender'   => $this->sender,
                                                                    'receiver' => $this->receiver,
