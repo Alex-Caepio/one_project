3699a909f758dbc075b2cf37c24c7d3a2bf7d5ed
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-05-12 14:51:43 +0100

    upcoming filtering

--- app/Http/Controllers/BookingMyClientController.php
+++ app/Http/Controllers/BookingMyClientController.php
@@ -11,37 +11,28 @@ use App\Transformers\MyClientClosedTransformer;
 use App\Transformers\MyClientPurchaseTransformer;
 use App\Transformers\MyClientTransformer;
 use App\Transformers\MyClientUpcomingTransformer;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
-class BookingMyClientController extends Controller
-{
+class BookingMyClientController extends Controller {
 
-    public function index(Request $request)
-    {
-        $query = User::query()->selectRaw(
-            'users.*,
+    public function index(Request $request) {
+        $query = User::query()->selectRaw('users.*,
             count(distinct live_bookings.id) live_bookings,
             count(distinct attended_bookings.id) attended_bookings,
             count(distinct bookings.id) bookings,
             max(bookings.created_at) last_purchase,
             max(attended_bookings.created_at) last_service
-            '
-        )
-            ->join('bookings', 'bookings.user_id', '=', 'users.id')
-            ->leftJoin('bookings as live_bookings',
-                function ($q) {
-                    $q->on('live_bookings.id', '=', 'bookings.id')
-                        ->on('live_bookings.datetime_from', '>', DB::raw('now()'));
-                })
-            ->leftJoin('bookings as attended_bookings',
-                function ($q) {
-                    $q->on('attended_bookings.id', '=', 'bookings.id')
-                        ->on('attended_bookings.datetime_from', '<=', DB::raw('now()'));
-                })
-            ->join('schedules', 'schedules.id', '=', 'bookings.schedule_id')
-            ->join('services', 'services.id', '=', 'schedules.service_id')
-            ->where('services.user_id', $request->user()->id)
-            ->groupBy('users.id');
+            ')->join('bookings', 'bookings.user_id', '=', 'users.id')
+                     ->leftJoin('bookings as live_bookings', function($q) {
+                         $q->on('live_bookings.id', '=', 'bookings.id')
+                           ->on('live_bookings.datetime_from', '>', DB::raw('now()'));
+                     })->leftJoin('bookings as attended_bookings', function($q) {
+                $q->on('attended_bookings.id', '=', 'bookings.id')
+                  ->on('attended_bookings.datetime_from', '<=', DB::raw('now()'));
+            })->join('schedules', 'schedules.id', '=', 'bookings.schedule_id')
+                     ->join('services', 'services.id', '=', 'schedules.service_id')
+                     ->where('services.user_id', $request->user()->id)->groupBy('users.id');
 
         if ($request->hasOrderBy()) {
             $order = $request->getOrderBy();
@@ -51,45 +42,40 @@ class BookingMyClientController extends Controller
         $paginator = $query->paginate($request->getLimit());
         $myClients = $paginator->getCollection();
 
-        return response(fractal($myClients, new MyClientTransformer())
-            ->parseIncludes($request->getIncludes()))
-            ->withPaginationHeaders($paginator);
+        return response(fractal($myClients,
+                                new MyClientTransformer())->parseIncludes($request->getIncludes()))->withPaginationHeaders($paginator);
     }
 
-    public function upcoming(Request $request)
-    {
+    public function upcoming(Request $request) {
         $paginator = Schedule::query()->selectRaw(implode(', ', [
-                'schedules.id as id',
-                'services.id as service_id',
-                'services.title as service_name',
-                'service_types.name as service_type',
-                'schedules.title as schedule_name',
-                'schedules.start_date as start_datetime',
-                'concat(count(bookings.id), " of ", schedules.attendees) as bookings',
-                'concat(count(bookings.id), " of ", count(bookings.id)) as full_paid',
-                'schedules.refund_terms as refund_terms',
-            ]))->join('services', 'services.id', '=', 'schedules.service_id')
+            'schedules.id as id',
+            'services.id as service_id',
+            'services.title as service_name',
+            'service_types.name as service_type',
+            'schedules.title as schedule_name',
+            'schedules.start_date as start_datetime',
+            'concat(count(bookings.id), " of ", schedules.attendees) as bookings',
+            'concat(count(bookings.id), " of ", count(bookings.id)) as full_paid',
+            'schedules.refund_terms as refund_terms',
+        ]))->join('services', 'services.id', '=', 'schedules.service_id')
                              ->join('service_types', 'service_types.id', '=', 'services.service_type_id')
                              ->join('bookings', static function($join) {
                                  $join->on(function($join) {
                                      $join->on('bookings.schedule_id', '=', 'schedules.id')
                                           ->whereNotIn('bookings.status', ['canceled', 'completed']);
                                  });
-                             })->where('services.user_id', $request->user()->id)
-                             ->where('bookings.datetime_from', '>', DB::raw('now()'))->groupBy('schedules.id')
-                             ->paginate($request->getLimit());
+                             })->where('services.user_id', $request->user()->id)->where(function($query) {
+                $query->where('bookings.datetime_from', '>=', Carbon::now())->orWhereNull('services.service_type_id', 'bespoke');
+            })->groupBy('schedules.id')->paginate($request->getLimit());
 
         $schedules = $paginator->getCollection();
 
-        return response(fractal($schedules, new MyClientUpcomingTransformer())
-            ->parseIncludes($request->getIncludes())->toArray())
-            ->withPaginationHeaders($paginator);
+        return response(fractal($schedules, new MyClientUpcomingTransformer())->parseIncludes($request->getIncludes())
+                                                                              ->toArray())->withPaginationHeaders($paginator);
     }
 
-    public function closed(Request $request)
-    {
-        $paginator = Booking::query()
-            ->selectRaw(implode(', ', [
+    public function closed(Request $request) {
+        $paginator = Booking::query()->selectRaw(implode(', ', [
                 'schedules.id as id',
                 'services.title as service_name',
                 'services.id as service_id',
@@ -101,27 +87,22 @@ class BookingMyClientController extends Controller
                 'purchases.price as paid',
                 'bookings.datetime_to as closure_date',
                 '"Complete" as status',
-            ]))
-            ->join('schedules', 'schedules.id', '=', 'bookings.schedule_id')
-            ->join('services', 'services.id', '=', 'schedules.service_id')
-            ->join('service_types', 'service_types.id', '=', 'services.service_type_id')
-            ->join('purchases', 'purchases.id', '=', 'bookings.purchase_id')
-            ->join('users', 'users.id', '=', 'bookings.user_id')
-            ->where('services.user_id', $request->user()->id)
-            ->where('bookings.datetime_from', '<=', DB::raw('now()'))
-            ->paginate($request->getLimit());
+            ]))->join('schedules', 'schedules.id', '=', 'bookings.schedule_id')
+                            ->join('services', 'services.id', '=', 'schedules.service_id')
+                            ->join('service_types', 'service_types.id', '=', 'services.service_type_id')
+                            ->join('purchases', 'purchases.id', '=', 'bookings.purchase_id')
+                            ->join('users', 'users.id', '=', 'bookings.user_id')
+                            ->where('services.user_id', $request->user()->id)
+                            ->where('bookings.datetime_from', '<=', DB::raw('now()'))->paginate($request->getLimit());
 
         $bookings = $paginator->getCollection();
 
-        return response(fractal($bookings, new MyClientClosedTransformer())
-            ->parseIncludes($request->getIncludes())->toArray())
-            ->withPaginationHeaders($paginator);
+        return response(fractal($bookings, new MyClientClosedTransformer())->parseIncludes($request->getIncludes())
+                                                                           ->toArray())->withPaginationHeaders($paginator);
     }
 
-    public function purchases(Request $request)
-    {
-        $paginator = Purchase::query()
-            ->selectRaw(implode(', ', [
+    public function purchases(Request $request) {
+        $paginator = Purchase::query()->selectRaw(implode(', ', [
                 'purchases.id as id',
                 'bookings.id as booking_id',
                 'bookings.reference as booking_reference',
@@ -135,19 +116,16 @@ class BookingMyClientController extends Controller
                 'schedules.location_displayed as location',
                 'schedules.refund_terms as refund_terms',
                 'bookings.reference as reference',
-            ]))
-            ->join('services', 'services.id', '=', 'purchases.service_id')
-            ->join('service_types', 'service_types.id', '=', 'services.service_type_id')
-            ->join('schedules', 'schedules.id', '=', 'purchases.schedule_id')
-            ->join('users', 'users.id', '=', 'purchases.user_id')
-            ->join('bookings', 'bookings.purchase_id', '=', 'purchases.id')
-            ->where('services.user_id', $request->user()->id)
-            ->paginate($request->getLimit());
+            ]))->join('services', 'services.id', '=', 'purchases.service_id')
+                             ->join('service_types', 'service_types.id', '=', 'services.service_type_id')
+                             ->join('schedules', 'schedules.id', '=', 'purchases.schedule_id')
+                             ->join('users', 'users.id', '=', 'purchases.user_id')
+                             ->join('bookings', 'bookings.purchase_id', '=', 'purchases.id')
+                             ->where('services.user_id', $request->user()->id)->paginate($request->getLimit());
 
         $purchases = $paginator->getCollection();
 
-        return response(fractal($purchases, new MyClientPurchaseTransformer())
-            ->parseIncludes($request->getIncludes()))
-            ->withPaginationHeaders($paginator);
+        return response(fractal($purchases,
+                                new MyClientPurchaseTransformer())->parseIncludes($request->getIncludes()))->withPaginationHeaders($paginator);
     }
 }
