05d5d5624353d5ca787d73f4c4e7972b8455912a
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-06-01 09:10:33 +0100

    validation in business media request

--- app/Http/Requests/Auth/UpdateMediaRequest.php
+++ app/Http/Requests/Auth/UpdateMediaRequest.php
@@ -37,33 +37,4 @@ class UpdateMediaRequest extends Request {
         ];
     }
 
-    public function withValidator($validator) {
-        $user = $this->user();
-        $validator->after(function($validator) use ($user) {
-            if ($this->getBoolFromRequest('is_published') === true || $user->is_published) {
-
-                if (!$user->business_name && !$this->business_name) {
-                    $validator->errors()->add('business_name', 'You have not filled in the field "Business name"');
-                }
-
-                if (!$user->business_address && !$this->business_address) {
-                    $validator->errors()
-                              ->add('business_address', 'You have not filled in the field "Business address"');
-                }
-
-                if (!$user->business_email && !$this->business_email) {
-                    $validator->errors()->add('business_email', 'You have not filled in the field "Business email"');
-                }
-
-                if (!$user->business_introduction && !$this->business_introduction) {
-                    $validator->errors()->add('business_introduction',
-                                              'You have not filled in the field "Business introduction"');
-                }
-
-                if (!$user->business_city && !$this->business_city) {
-                    $validator->errors()->add('business_city', 'You have not filled in the field "Business City"');
-                }
-            }
-        });
-    }
 }
