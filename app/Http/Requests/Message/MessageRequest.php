45fc7bacdc60e5c1798f52ad3d6eb1ba37f50060
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-06-03 14:27:11 +0100

    MessageValidator

--- app/Http/Requests/Message/MessageRequest.php
+++ app/Http/Requests/Message/MessageRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Message;
 
 use App\Http\Requests\Request;
+use App\Models\Booking;
 
 class MessageRequest extends Request
 {
@@ -11,9 +12,8 @@ class MessageRequest extends Request
      *
      * @return bool
      */
-    public function authorize()
-    {
-        return $this->user()->id != $this->user->id ;
+    public function authorize() {
+        return $this->user()->id !== $this->user->id;
     }
 
     /**
@@ -26,5 +26,26 @@ class MessageRequest extends Request
         return [
             'text' => 'max:1000'
         ];
+    }
+
+    public function withValidator($validator) {
+        $validator->after(function($validator) {
+            // practitioner as sender
+            if ($this->user()->isPractitioner()) {
+                if (!$this->user()->is_published && !Booking::where('practitioner_id', $this->user()->id)->where('user_id', $this->user->id)->active()->exists()) {
+                    $validator
+                        ->errors()
+                        ->add('text',
+                              'You are not allowed to send message to the client not in your clients list');
+                }
+            } else {
+                if (!$this->user->is_published && !Booking::where('practitioner_id', $this->user->id)->where('user_id', $this->user()->id)->active()->exists()) {
+                    $validator
+                        ->errors()
+                        ->add('text',
+                              'You are not allowed to send message to this practitioner');
+                }
+            }
+        });
     }
 }
