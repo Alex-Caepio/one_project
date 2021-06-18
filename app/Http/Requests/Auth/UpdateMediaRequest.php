568fcdbe84a2019ccb68a9ba31dcd160cec7f93b
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-06-18 11:25:56 +0100

    UpdateMediaRequest from front/admin parts

--- app/Http/Requests/Auth/UpdateMediaRequest.php
+++ app/Http/Requests/Auth/UpdateMediaRequest.php
@@ -3,13 +3,13 @@
 namespace App\Http\Requests\Auth;
 
 use App\Http\Requests\Request;
+use App\Models\User;
 use App\Traits\PublishPractitionerRequestValidatorTrait;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class UpdateMediaRequest extends Request {
 
-    use PublishPractitionerRequestValidatorTrait;
     /**
      * Authorization rules
      *
@@ -29,7 +29,7 @@ class UpdateMediaRequest extends Request {
             'background_url'        => 'nullable|url',
             'business_city'         => 'required_if:is_published,true|string|max:150',
             'business_name'         => 'required_if:is_published,true|max:255|min:2',
-            'business_introduction' => 'sometimes|string|max:150',
+            'business_introduction' => 'required_if:is_published,true|string|max:150',
             'business_country_id'   => 'required|exists:countries,id|integer',
             'slug'                  => [
                 Rule::unique('users', 'slug')->ignore($this->user()->id),
@@ -40,7 +40,30 @@ class UpdateMediaRequest extends Request {
 
 
     public function withValidator($validator) {
-       $this->validatePublishState($validator);
+        $validator->after(function($validator) {
+            if ($this->route()->getName() === self::$adminRouteName) {
+                $practitioner = $this->practitioner;
+            } else {
+                $practitioner = Auth::user();
+            }
+            if ($practitioner instanceof User) {
+                $validator->setRules([
+                                         'business_address'            => 'required|max:255',
+                                         'business_email'              => 'required|max:255|email',
+                                         'business_phone_number'       => 'required|digits_between:2,255|numeric',
+                                         'business_phone_country_code' => 'required|exists:countries,id|integer',
+                                     ])->setData([
+                                                     'business_address'            => $practitioner->business_address,
+                                                     'business_email'              => $practitioner->business_email,
+                                                     'business_phone_number'       => $practitioner->business_phone_number,
+                                                     'business_phone_country_code' => $practitioner->business_phone_country_code,
+                                                 ])->validate();
+
+            } else {
+                abort(403, 'Practitioner is not defined');
+            }
+
+        });
     }
 
 }
