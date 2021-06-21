696bf212029a653a14c5bc6eb8aaeba4cd100251
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-06-21 20:11:13 +0100

    available till validation

--- app/Http/Requests/Schedule/RetreatScheduleRequest.php
+++ app/Http/Requests/Schedule/RetreatScheduleRequest.php
@@ -28,7 +28,7 @@ class RetreatScheduleRequest extends GenericSchedule {
             }),
             'prices.*.cost'           => 'required_if:prices.*.is_free,false',
             'prices.*.is_free'        => 'required',
-            'prices.*.available_till' => 'before:end_date',
+            'prices.*.available_till' => 'nullabe|before:end_date',
 
             'deposit_amount'     => 'required_if:deposit_accepted,true',
             'deposit_final_date' => 'required_if:deposit_accepted,true',
