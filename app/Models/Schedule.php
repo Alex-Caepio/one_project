7ec3e2705d6129ef1d3d8a14d7af64412196373a
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-06-23 10:23:18 +0100

    add published trait

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\PublishedScope;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -23,7 +24,7 @@ use Illuminate\Support\Facades\Log;
  * @property Service service
  */
 class Schedule extends Model {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, PublishedScope;
 
     protected $fillable = [
         'title',
