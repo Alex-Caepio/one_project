9d521934be04e214a7df24d159a00cdb539b0097
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-06-24 20:28:42 +0100

    Plan Purchase

---
+++ app/Actions/Plan/UpdateSubscription.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Actions\Plan;
+
+use App\Events\AccountUpgradedToPractitioner;
+use App\Events\ChangeOfSubscription;
+use App\Events\SubscriptionConfirmation;
+use App\Http\Requests\Request;
+use App\Models\Plan;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
+use Stripe\StripeClient;
+
+class UpdateSubscription {
+
+    public function execute(User $user, StripeClient $stripeClient, Plan $plan, bool $isNewPlan,
+                            Request $request): bool {
+        try {
+            if (!$plan->isActiveTrial()) {
+                $subscription = $stripeClient->subscriptions->create([
+                                                                         'default_payment_method' => $request->payment_method_id,
+                                                                         'customer'               => $user->stripe_customer_id,
+                                                                         'items'                  => [
+                                                                             ['price' => $plan->stripe_id],
+                                                                         ],
+                                                                     ]);
+                $user->stripe_plan_id = $subscription->id;
+                $user->plan_until = Carbon::createFromTimestamp($subscription->current_period_end);
+            } else {
+                $user->plan_id = $plan->id;
+                $user->plan_until = $plan->free_start_to;
+            }
+            $user->plan_from = Carbon::now();
+            $user->account_type = User::ACCOUNT_PRACTITIONER;
+
+            $isUpgradedToPractitioner = $user->isDirty('account_type');
+            if ($isUpgradedToPractitioner) {
+                $user->accepted_practitioner_agreement = true;
+            }
+            $user->save();
+
+            if ($isNewPlan) {
+                if ($isUpgradedToPractitioner) {
+                    event(new AccountUpgradedToPractitioner($user, $plan));
+                }
+                event(new SubscriptionConfirmation($user, $plan));
+            } else {
+                event(new ChangeOfSubscription($user, $plan, null));
+            }
+
+        } catch (\Exception $e) {
+            Log::channel('stripe_plans_errors')->info('Error purchasing a plan', [
+                'user_id'           => $user->id ?? null,
+                'plan_id'           => $plan->id ?? null,
+                'customer'          => $user->stripe_customer_id ?? null,
+                'stripe_plan_id'    => $subscription->id ?? null,
+                'payment_method_id' => $request->payment_method_id ?? null,
+                'price_stripe_id'   => $plan->stripe_id,
+                'isTrial'           => $plan->isActiveTrial(),
+                'message'           => $e->getMessage(),
+                'trace'             => $e->getTraceAsString(),
+            ]);
+            return false;
+
+        }
+
+        Log::channel('stripe_plans_success')->info('Plan successfully purchased', [
+            'user_id'           => $user->id,
+            'plan_id'           => $plan->id,
+            'customer'          => $user->stripe_customer_id,
+            'stripe_plan_id'    => $subscription->id ?? null,
+            'payment_method_id' => $request->payment_method_id ?? null,
+            'price_stripe_id'   => $plan->stripe_id,
+            'isTrial'           => $plan->isActiveTrial(),
+        ]);
+        return true;
+    }
+
+}
--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Plan\CancelSubscription;
+use App\Actions\Plan\UpdateSubscription;
 use App\Events\AccountUpgradedToPractitioner;
 use App\Events\ChangeOfSubscription;
 use App\Helpers\UserRightsHelper;
 use App\Http\Requests\Plans\PlanRequest;
 use App\Events\SubscriptionConfirmation;
+use App\Http\Requests\Plans\PlanTrialRequest;
 use App\Models\Article;
 use App\Models\Plan;
 use App\Http\Requests\Request;
@@ -26,75 +29,12 @@ class PlanController extends Controller {
 
     public function purchase(Plan $plan, StripeClient $stripe, PlanRequest $request) {
         $user = Auth::user();
-        $previousPlan = $user->plan;
-        $isNewPlan = empty($user->plan_id);
 
+        run_action(CancelSubscription::class, $user, $stripe);
 
-        if (!empty($user->stripe_plan_id)) {
-            $logData = [
-                'user_id'                => $user->id,
-                'plan_id'                => $plan->id,
-                'customer'               => $user->stripe_customer_id,
-                'stripe_subscription_id' => $user->stripe_plan_id,
-                'price_stripe_id'        => $plan->stripe_id
-            ];
-            try {
-                $stripe->subscriptions->cancel($user->stripe_plan_id, []);
-                Log::channel('stripe_plans_success')->info('Plan successfully cancelled', $logData);
-            } catch (\Exception $e) {
-                $logData['error'] = $e->getMessage();
-                Log::channel('stripe_plans_success')->info('Plan cancellation error purchased', $logData);
-            } finally {
-                $user->stripe_plan_id = null;
-                $user->plan_id = null;
-                $user->plan_until = null;
-                $user->plan_from = null;
-                $user->save();
-            }
-        }
+        $result = run_action(UpdateSubscription::class, $user, $stripe, $plan, empty($user->plan_id), $request);
 
-        try {
-
-            $subscription = $stripe->subscriptions->create([
-                                                               'default_payment_method' => $request->payment_method_id,
-                                                               'customer'               => $user->stripe_customer_id,
-                                                               'items'                  => [
-                                                                   ['price' => $plan->stripe_id],
-                                                               ],
-                                                           ]);
-            $user->stripe_plan_id = $subscription->id;
-            $user->plan_id = $plan->id;
-            $user->plan_until = Carbon::createFromTimestamp($subscription->current_period_end);
-            $user->plan_from = Carbon::now();
-            $user->account_type = User::ACCOUNT_PRACTITIONER;
-            $isUpgradedToPractitioner = $user->isDirty('account_type');
-            if ($isUpgradedToPractitioner) {
-                $user->accepted_practitioner_agreement = true;
-            }
-            $user->save();
-
-            if ($isNewPlan) {
-                if ($isUpgradedToPractitioner) {
-                    event(new AccountUpgradedToPractitioner($user, $plan));
-                }
-                event(new SubscriptionConfirmation($user, $plan));
-            } else {
-                event(new ChangeOfSubscription($user, $plan, $previousPlan));
-            }
-
-        } catch (\Exception $e) {
-            Log::channel('stripe_plans_errors')->info('Error purchasing a plan', [
-                'user_id'           => $user->id ?? null,
-                'plan_id'           => $plan->id ?? null,
-                'customer'          => $user->stripe_customer_id ?? null,
-                'stripe_plan_id'    => $subscription->id ?? null,
-                'payment_method_id' => $request->payment_method_id ?? null,
-                'price_stripe_id'   => $plan->stripe_id ?? null,
-                'message'           => $e->getMessage(),
-                'trace'             => $e->getTraceAsString(),
-            ]);
-
-
+        if (!$result) {
             return response()->json([
                                         'errors' => [
                                             'payment_method_id' => 'The payment could not be processed. Please check with your bank or choose another payment option.'
@@ -102,14 +42,25 @@ class PlanController extends Controller {
                                     ], 422);
         }
 
-        Log::channel('stripe_plans_success')->info('Plan successfully purchased', [
-            'user_id'           => $user->id,
-            'plan_id'           => $plan->id,
-            'customer'          => $user->stripe_customer_id,
-            'stripe_plan_id'    => $subscription->id,
-            'payment_method_id' => $request->payment_method_id,
-            'price_stripe_id'   => $plan->stripe_id
-        ]);
+        return response('', 204);
+
+    }
+
+    public function purchaseFree(Plan $plan, StripeClient $stripe, PlanTrialRequest $request) {
+        $user = Auth::user();
+
+        run_action(CancelSubscription::class, $user, $stripe);
+
+        $result = run_action(UpdateSubscription::class, $user, $stripe, $plan, empty($user->plan_id), $request);
+
+        if (!$result) {
+            return response()->json([
+                                        'errors' => [
+                                            'payment_method_id' => 'The payment could not be processed. Please check with your bank or choose another payment option.'
+                                        ]
+                                    ], 422);
+        }
+
         return response('', 204);
 
     }
