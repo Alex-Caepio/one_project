ddd4efb55ebd66dba014e447e7af83583c7e0cd9
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-07-19 16:23:26 +0300

    MyClients filtrator

--- app/Filters/UserFiltrator.php
+++ app/Filters/UserFiltrator.php
@@ -3,12 +3,20 @@
 namespace App\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use App\Http\Requests\Request;
 
 class UserFiltrator {
 
     public function apply(Builder $queryBuilder, Request $request) {
+
+        $isClient = $request->getBoolFromRequest('is_client');
+        if ($isClient) {
+            $queryBuilder->whereHas('bookings', static function($query) {
+                $query->where('bookings.practitioner_id', Auth::id());
+            });
+        }
 
         $isAuthor = $request->getBoolFromRequest('is_author');
         if ($isAuthor !== null) {
@@ -28,10 +36,8 @@ class UserFiltrator {
             $search = '%' . trim($request->get('search')) . '%';
             $emailReplace = str_replace(' ', '+', $search);
             $queryBuilder->where(function($query) use ($search, $emailReplace) {
-                $query->where('first_name', 'LIKE', $search)
-                      ->orWhere('last_name', 'LIKE', $search)
-                      ->orWhere('business_email', 'LIKE', $emailReplace)
-                      ->orWhere('email', 'LIKE', $emailReplace)
+                $query->where('first_name', 'LIKE', $search)->orWhere('last_name', 'LIKE', $search)
+                      ->orWhere('business_email', 'LIKE', $emailReplace)->orWhere('email', 'LIKE', $emailReplace)
                       ->orWhere('business_name', 'LIKE', $search);
             });
         }
