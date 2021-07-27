3f077aa278102304ad7b13e86671ff6fa123e3f7
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-07-27 13:54:54 +0300

    Mark as read dummy fix

--- app/Http/Requests/Notification/MarkAsReadRequest.php
+++ app/Http/Requests/Notification/MarkAsReadRequest.php
@@ -6,23 +6,20 @@ namespace App\Http\Requests\Notification;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
-class MarkAsReadRequest extends FormRequest
-{
+class MarkAsReadRequest extends FormRequest {
     /**
      * Authorization rules
      *
      * @return bool
      */
-    public function authorize()
-    {
-        return $this->notification->receiver_id === Auth::id();
+    public function authorize() {
+        return $this->notification->receiver_id === Auth::user()->id;
     }
 
     /**
      * @return array
      */
-    public function rules()
-    {
+    public function rules() {
         return [];
     }
 
