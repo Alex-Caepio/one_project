61cf6e38c84bb66695dd274b44f1a0c2ee67cefa
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-09-15 12:27:03 +0300

    Purchase number available

--- app/Http/Requests/Schedule/PurchaseScheduleRequest.php
+++ app/Http/Requests/Schedule/PurchaseScheduleRequest.php
@@ -61,7 +61,7 @@ class PurchaseScheduleRequest extends Request implements CreateScheduleInterface
             $bookingsCount = Booking::where('price_id', $this->price_id)->uncanceled()->count();
             $requiredAmount = (int)$this->request->get('amount');
 
-            if ($schedule->service->service_type_id !== 'appointment' && $price->number_available !== null) {
+            if ($schedule->service->service_type_id !== 'appointment' && (int)$price->number_available > 0) {
                 if (($bookingsCount + $requiredAmount) >= (int)$price->number_available) {
                     $validator->errors()->add('price_id', 'All schedules for that price were sold out');
                 }
