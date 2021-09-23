fa3bf392070303d276d1fc38a0e0d4266b9291c2
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-09-23 13:49:15 +0100

    useless code

--- app/Actions/Schedule/ScheduleUpdate.php
+++ app/Actions/Schedule/ScheduleUpdate.php
@@ -4,12 +4,7 @@
 namespace App\Actions\Schedule;
 
 
-use App\Http\Requests\Request;
-use App\Http\Requests\Schedule\CreateScheduleInterface;
-use App\Http\Requests\Schedule\GenericUpdateSchedule;
 use App\Models\Schedule;
-use App\Models\Service;
-use Illuminate\Support\Facades\Log;
 
 class ScheduleUpdate extends ScheduleSave {
 
