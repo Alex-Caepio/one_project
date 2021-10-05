70f0b8a5f09f9e4114b7ffe88611750b02c52690
Kate Nikolaeva <nikolaeva.ekaterina.vl@gmail.com>
2021-10-05 08:12:28 +0100

    installments count

--- app/Transformers/MyClientUpcomingTransformer.php
+++ app/Transformers/MyClientUpcomingTransformer.php
@@ -14,16 +14,17 @@ class MyClientUpcomingTransformer extends Transformer
     public function transform($myClientPurchase)
     {
         return [
-            'id'               => $myClientPurchase->id,
-            'service_id'       => $myClientPurchase->service_id,
-            'service_name'     => $myClientPurchase->service_name,
-            'service_type'     => $myClientPurchase->service_type,
-            'schedule_name'    => $myClientPurchase->schedule_name,
-            'start_datetime'   => new Carbon($myClientPurchase->start_datetime),
-            'bookings'         => $myClientPurchase->bookings,
-            'has_installments' => (int)$myClientPurchase->bookings_with_installment > 0,
-            'full_paid'        => $myClientPurchase->full_paid,
-            'refund_terms'     => $myClientPurchase->refund_terms,
+            'id'                 => $myClientPurchase->id,
+            'service_id'         => $myClientPurchase->service_id,
+            'service_name'       => $myClientPurchase->service_name,
+            'service_type'       => $myClientPurchase->service_type,
+            'schedule_name'      => $myClientPurchase->schedule_name,
+            'start_datetime'     => new Carbon($myClientPurchase->start_datetime),
+            'bookings'           => $myClientPurchase->bookings,
+            'has_installments'   => (int)$myClientPurchase->bookings_with_installment > 0,
+            'installments_count' => $myClientPurchase->bookings_with_installment,
+            'full_paid'          => $myClientPurchase->full_paid,
+            'refund_terms'       => $myClientPurchase->refund_terms,
         ];
     }
 }
