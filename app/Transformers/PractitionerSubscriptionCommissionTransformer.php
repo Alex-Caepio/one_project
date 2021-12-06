32c69dda0e62cb2f8534723d8bda3f26aa40cee7
Volodymyr Lantsev <adminko@artjoker.net>
2021-12-06 09:40:52 +0200

    #31 cleanup

--- app/Transformers/PractitionerSubscriptionCommissionTransformer.php
+++ app/Transformers/PractitionerSubscriptionCommissionTransformer.php
@@ -20,16 +20,16 @@ class PractitionerSubscriptionCommissionTransformer extends Transformer
     public function transform(PractitionerSubscriptionCommission $commission)
     {
         return [
-            'id'                 => $commission->id,
-            'user_id'            => $commission->user_id,
-            'rate'               => $commission->rate,
+            'id' => $commission->id,
+            'user_id' => $commission->user_id,
+            'rate' => $commission->rate,
             'commission_on_sale' => $commission->commission_on_sale,
-            'date_from'          => is_null($commission->date_from) ? null : $commission->date_from->format('Y-m-d'),
-            'date_to'            => is_null($commission->date_to) ? null : $commission->date_to->format('Y-m-d'),
-            'is_dateless'        => $commission->is_dateless,
-            'created_at'         => $commission->created_at,
-            'updated_at'         => $commission->updated_at,
-            'stripe_coupon_id'   => $commission->stripe_coupon_id,
+            'date_from' => is_null($commission->date_from) ? null : $commission->date_from->format('Y-m-d'),
+            'date_to' => is_null($commission->date_to) ? null : $commission->date_to->format('Y-m-d'),
+            'is_dateless' => $commission->is_dateless,
+            'created_at' => $commission->created_at,
+            'updated_at' => $commission->updated_at,
+            'stripe_coupon_id' => $commission->stripe_coupon_id,
             'subscription_schedule_id' => $commission->subscription_schedule_id,
         ];
     }
