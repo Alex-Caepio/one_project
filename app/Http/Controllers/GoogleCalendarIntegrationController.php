ed7fcc1d6c2523c4c37e8185fa822f648fd466e6
Volodymyr Lantsev <adminko@artjoker.net>
2021-12-21 10:39:28 +0200

    fix unavailability calendar

--- app/Http/Controllers/GoogleCalendarIntegrationController.php
+++ app/Http/Controllers/GoogleCalendarIntegrationController.php
@@ -13,53 +13,72 @@ use App\Models\UserUnavailabilities;
 use App\Transformers\GoogleCalendarEventTransformer;
 use App\Transformers\GoogleCalendarTransformer;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 
-class GoogleCalendarIntegrationController extends Controller {
+class GoogleCalendarIntegrationController extends Controller
+{
 
-    public function updateSettings(SettingsRequest $request) {
+    public function updateSettings(SettingsRequest $request)
+    {
         $user = Auth::user();
         $calendar = !$user->calendar ? GoogleCalendar::createDefaultModel($user) : $user->calendar;
         $calendar->timezone_id = $request->get('timezone_id');
         $calendar->save();
+
         if ($request->filled('unavailabilities')) {
             UserUnavailabilities::where('practitioner_id', $user->id)->delete();
-            $requestData = $request->get('unavailabilities');
-            $additionalParams = ['practitioner_id' => $user->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()];
-            $dataToInsert = array_map(static function($value) use($additionalParams) {
-                return array_merge($value, $additionalParams);
-            }, $requestData);
-            UserUnavailabilities::insert($dataToInsert);
+            $now = Carbon::now()->toDateTimeString();
+            $dataToInsert = collect($request->get('unavailabilities'))->map(function ($value) use ($now, $user) {
+                return [
+                    'practitioner_id' => $user->id,
+                    'start_date' => Carbon::parse($value['start_date'])->toDateTimeString(),
+                    'end_date' => Carbon::parse($value['end_date'])->toDateTimeString(),
+                    'created_at' => $now,
+                    'updated_at' => $now
+                ];
+            });
+            UserUnavailabilities::insert($dataToInsert->toArray());
         } else {
             UserUnavailabilities::where('practitioner_id', $user->id)->delete();
         }
+
         if ($calendar->calendar_id) {
             try {
                 $gcHelper = new GoogleCalendarHelper($calendar);
                 $gcHelper->updateTimezone();
-            } catch (\Exception $e) {
-                Log::channel('google_authorisation_failed')->info('Unable to update timezone in calendar:', [
-                    'calendar_id' => $calendar->calendar_id,
-                    'user_id'     => $user->id,
-                    'message'     => $e->getMessage()
-                ]);
+            } catch (Exception $e) {
+                Log::channel('google_authorisation_failed')
+                    ->info('Unable to update timezone in calendar:', [
+                        'calendar_id' => $calendar->calendar_id,
+                        'user_id' => $user->id,
+                        'message' => $e->getMessage()
+                    ]);
             }
         }
         $calendar->load('unavailabilities');
-        return response(fractal($calendar, new GoogleCalendarTransformer())->parseIncludes(['unavailabilities'])->toArray());
+
+        return response(
+            fractal($calendar, new GoogleCalendarTransformer())->parseIncludes(['unavailabilities'])->toArray()
+        );
     }
 
-    public function getSettings(Request $request) {
+    public function getSettings(Request $request)
+    {
         $user = Auth::user();
         $user->load('calendar');
-        return response(fractal($user->calendar, new GoogleCalendarTransformer())
-                            ->parseIncludes($request->getIncludes())->toArray());
+
+        return response(
+            fractal($user->calendar, new GoogleCalendarTransformer())
+                ->parseIncludes($request->getIncludes())->toArray()
+        );
     }
 
-    public function auth(AuthRequest $request) {
+    public function auth(AuthRequest $request)
+    {
         $user = Auth::user();
         $calendar = !$user->calendar ? GoogleCalendar::createDefaultModel($user) : $user->calendar;
 
@@ -76,10 +95,14 @@ class GoogleCalendarIntegrationController extends Controller {
         abort(500, 'Google authorization failed');
     }
 
-    public function getEventList(EventListRequest $request) {
+    public function getEventList(EventListRequest $request)
+    {
         $gcHelper = new GoogleCalendarHelper(Auth::user()->calendar);
-        return response(fractal($gcHelper->getEventList(), new GoogleCalendarEventTransformer())
-                            ->parseIncludes($request->getIncludes())->toArray());
+
+        return response(
+            fractal($gcHelper->getEventList(), new GoogleCalendarEventTransformer())
+                ->parseIncludes($request->getIncludes())->toArray()
+        );
     }
 
 }
