81d44d3444fe6c0f575baf7c835ed92958c0371d
Volodymyr Lantsev <adminko@artjoker.net>
2021-12-24 10:04:37 +0200

    calendar pass schedule into calendar

--- app/Http/Requests/CalendarSettings/EventListRequest.php
+++ app/Http/Requests/CalendarSettings/EventListRequest.php
@@ -7,14 +7,15 @@ use App\Http\Requests\Request;
 use App\Models\GoogleCalendar;
 use Illuminate\Support\Facades\Auth;
 
-class EventListRequest extends Request {
+class EventListRequest extends Request
+{
 
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -24,11 +25,17 @@ class EventListRequest extends Request {
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            'first_date_point' => 'required|date_format:Y-m-d H:i:s',
-            'last_date_point' => 'required|date_format:Y-m-d H:i:s'
+            'first_date_point' => [
+                'required',
+                'date_format:Y-m-d H:i:s'
+            ],
+            'last_date_point' => [
+                'required',
+                'date_format:Y-m-d H:i:s'
+            ]
         ];
     }
 
--- app/Transformers/CalendarEventTransformer.php
+++ app/Transformers/CalendarEventTransformer.php
@@ -4,32 +4,21 @@ namespace App\Transformers;
 
 use App\Models\Booking;
 
-class CalendarEventTransformer extends Transformer {
+class CalendarEventTransformer extends Transformer
+{
 
-    public function transform(Booking $event) {
+    public function transform(Booking $event): array
+    {
         return [
-            'id'         => $event->id,
+            'id' => $event->id,
             'start_date' => $event->datetime_from,
-            'end_date'   => $event->datetime_to,
-            'location'   => $event->schedule->location_displayed,
-            'summary'    => $event->schedule->title,
-            //'clients'    => $this->getClientsArray($event->clients)
+            'end_date' => $event->datetime_to,
+            'location' => $event->schedule->location_displayed,
+            'summary' => $event->schedule->title,
+            'resource' => [
+                'schedule_id' => $event->schedule_id
+            ]
         ];
     }
-
-
-    /**
-     * @deprecated
-     * @param null|array $clients
-     * @return array
-     */
-    private function getClientsArray(?array $clients): array {
-        $result = [];
-        foreach ($clients as $client) {
-            $result[] = ['fullname' => $client->getDisplayName(), 'email' => $client->getEmail()];
-        }
-        return $result;
-    }
-
 
 }
