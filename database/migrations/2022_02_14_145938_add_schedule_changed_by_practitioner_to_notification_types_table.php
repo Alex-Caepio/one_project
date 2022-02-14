0e88d93066b2d056b4644cb376c535bb46ae95c0
Aleh Dzmitrachenka <aleh.dzmitrachenka@wbstraining.de>
2022-02-14 18:01:51 +0300

    166: add new schedule_changed_by_practitioner

---
+++ database/migrations/2022_02_14_145938_add_schedule_changed_by_practitioner_to_notification_types_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddScheduleChangedByPractitionerToNotificationTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE `notifications` CHANGE `type` `type` ENUM('booking_canceled_by_client',
+            'booking_canceled_by_practitioner',
+            'amendment_canceled_by_client',
+            'declined_by_client',
+            'rescheduled_by_client',
+            'rescheduled_by_practitioner',
+            'schedule_changed_by_practitioner'
+        );");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("ALTER TABLE `notifications` CHANGE `type` `type` ENUM(
+            'booking_canceled_by_client',
+            'booking_canceled_by_practitioner',
+            'amendment_canceled_by_client',
+            'declined_by_client',
+            'rescheduled_by_client',
+            'rescheduled_by_practitioner'
+        );");
+    }
+}
