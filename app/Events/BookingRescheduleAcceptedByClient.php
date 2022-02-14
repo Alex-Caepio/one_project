d3f917d0672c5fc641075ad44608447721c64569
Aleh Dzmitrachenka <aleh.dzmitrachenka@wbstraining.de>
2022-02-14 18:02:08 +0300

    166: update code style

--- app/Events/BookingRescheduleAcceptedByClient.php
+++ app/Events/BookingRescheduleAcceptedByClient.php
@@ -10,13 +10,16 @@ use Illuminate\Queue\SerializesModels;
 use App\Models\Notification;
 use App\Models\RescheduleRequest;
 
-class BookingRescheduleAcceptedByClient {
+class BookingRescheduleAcceptedByClient
+{
     use Dispatchable, InteractsWithSockets, SerializesModels, EventFillableFromBooking;
 
     public User $recipient;
+
     public bool $informPractitioner;
 
-    public function __construct(Booking $booking, bool $informPractitioner = true) {
+    public function __construct(Booking $booking, bool $informPractitioner = true)
+    {
         $this->booking = $booking;
         $this->informPractitioner = $informPractitioner;
         $this->fillEvent();
