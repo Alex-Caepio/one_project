06191b9d4e72de3190aa7eae3add122c55e4528c
Bulat <botton95@gmail.com>
2022-04-12 15:57:40 +0300

    Changed model relation

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -87,7 +87,7 @@ class Service extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function practitioner()
