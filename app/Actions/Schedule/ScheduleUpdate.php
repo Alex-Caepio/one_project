91cd7c933c6c171feb5bc3c4e4ede005f2fa79c1
Aleh Dzmitrachenka <a.dzmitrachenka@andersenlab.com>
2022-07-22 14:30:01 +0300

    570: update styles

--- app/Actions/Schedule/ScheduleUpdate.php
+++ app/Actions/Schedule/ScheduleUpdate.php
@@ -1,20 +1,19 @@
 <?php
 
-
 namespace App\Actions\Schedule;
-
 
 use App\Models\Schedule;
 
 class ScheduleUpdate extends ScheduleSave
 {
-
     public function execute($request, Schedule $schedule): Schedule
     {
         $data = $this->collectRequest($request, $schedule->service);
+
         if ($this->isFilesUpdated($data['schedule_files'], $schedule)) {
             $schedule->isScheduleFilesUpdated = true;
         }
+
         $schedule->update($data);
         $this->updatePrices($request, $schedule);
         $this->saveRelations($request, $schedule);
@@ -31,17 +30,16 @@ class ScheduleUpdate extends ScheduleSave
 
     private function isFilesUpdated(array $files, Schedule $schedule): bool
     {
-        $newFilesArr = array_map(function ($el) {
-            return $el['url'];
-        }, $files);
-
+        $newFilesArr = array_column($files, 'url');
         $oldFilesArr = $schedule->schedule_files->pluck('url')->toArray();
+
         if (
             !empty(array_diff($newFilesArr, $oldFilesArr))
             || !empty(array_diff($oldFilesArr, $newFilesArr))
         ) {
             return true;
         }
+
         return false;
     }
 }
