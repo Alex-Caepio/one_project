e1e8c2582ea278e212fb91f63a02a8b889441a37
Dmitriy <dima.kacyuk@gmail.com>
2022-09-09 16:02:08 +0300

    fix(back): fix service update email 99

--- app/Listeners/SendServiceNotification.php
+++ app/Listeners/SendServiceNotification.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\ServiceUpdated;
 use App\Events\ServiceUpdatedNonContractual;
 use App\Models\Service;
+use DateTime;
 
 /**
  * Handles events and sends an email when a service has been updated and has
@@ -14,10 +15,17 @@ class SendServiceNotification
 {
     public function handle(ServiceUpdated $event)
     {
+        $lastPublish = new DateTime(date(
+            'Y-m-d H:i:s',
+            strtotime('+5 minutes', strtotime($event->service->last_published))
+        ));
+        $currentDate = new DateTime('now');
+
         if (
             $event->service->hasUpdates()
             && !$event->service->visibilityChanged()
             && !in_array($event->service->service_type->id, [Service::TYPE_BESPOKE])
+            && $lastPublish < $currentDate
         ) {
             event(new ServiceUpdatedNonContractual($event->service));
         }
