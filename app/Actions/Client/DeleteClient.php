927975eeff8b72c975e458dee7101d1091af995f
Dmitriy <dima.kacyuk@gmail.com>
2022-10-10 12:47:24 +0300

    fix(back): fix admin terminate accounts

--- app/Actions/Client/DeleteClient.php
+++ app/Actions/Client/DeleteClient.php
@@ -10,5 +10,7 @@ class DeleteClient
     public function execute(User $user, string $reason): void
     {
         run_action(DeleteUser::class, $user, $reason);
+        $user->delete();
+        run_action(CancelClientBookings::class, $user);
     }
 }
